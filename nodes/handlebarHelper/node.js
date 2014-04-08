535604b6ad1ccac76ec0f4abf49735b9fe0cd4b8
Rob Halff <rob.halff@gmail.com>
2014-04-09 01:57:49 +0200

    add handlebars helper

---
+++ nodes/handlebarHelper/node.js
@@ -0,0 +1,2 @@
+handlebars.registerHelper(input.name, input.fn);
+output.handlebars = handlebars;
--- nodes/handlebars/node.js
+++ nodes/handlebars/node.js
@@ -1,4 +1,5 @@
-var tpl = handlebars.compile(input.body);
+var hb = input.handlebars || handlebars;
+var tpl = hb.compile(input.body);
 output = {
   out: tpl(input.vars)
 }
