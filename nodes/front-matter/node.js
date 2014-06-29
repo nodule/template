c3eecb721f7383287bda166f2c6afb1987a4dc3b
Rob Halff <rob.halff@gmail.com>
2014-06-29 13:34:38 +0200

    update

--- nodes/front-matter/node.js
+++ nodes/front-matter/node.js
@@ -1,9 +1,3 @@
 on.input.in = function() {
   output( { out: front_matter(data) });
-
-  // really necessary, I would prefer a parameter to output(dat, true);
-  // maybe I could also check if the done argument is requested
-  // function(done).. ok appearently we want to determine ourselves
-  // when we are done.
-  done();
 };
