6621e0553fffa119d74db66c8c99e50fb27b8e88
Rob Halff <rob.halff@gmail.com>
2014-12-21 07:31:28 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  frontMatter:  require('./front-matter'),
-  handlebars:  require('./handlebars'),
-  handlebarsHelper:  require('./handlebarsHelper'),
-  jade:  require('./jade'),
-  markdown:  require('./markdown'),
-  mustache:  require('./mustache'),
-  yaml:  require('./yaml')
+  frontMatter: require('./front-matter'),
+  handlebars: require('./handlebars'),
+  handlebarsHelper: require('./handlebarsHelper'),
+  jade: require('./jade'),
+  markdown: require('./markdown'),
+  mustache: require('./mustache'),
+  yaml: require('./yaml')
 }
