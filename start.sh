f52abc9c597a83d02e6c58da99d6f5f01c882f58
Rob Halff <rob.halff@gmail.com>
2016-09-09 03:13:33 +0200

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 # TODO: Npm loading should work, otherwise their will be too much noise from unused components being send
 # DEBUG=* fbpx-server -L debug --loader npm --port 4000 --compat
-DEBUG=* fbpx-server -L debug --loader remote --port 4000 --compat
+# DEBUG=chix:* fbpx-server -L debug --loader remote --port 4000 --compat
+DEBUG=chix:* fbpx-server --loader remote --port 4000 --compat
 
